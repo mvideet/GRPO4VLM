7556095f56b34eeffe5d45e67c4573a909dd77c4
sagrawal0410 <agrawalshaurya1006@gmail.com>
2025-11-29 22:40:51 -0500

    run_maze fix

--- VLM_PPO_MAZE/scripts/run_maze.sh
+++ VLM_PPO_MAZE/scripts/run_maze.sh
@@ -26,6 +26,6 @@ TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES="0,1" accelerate launch --conf
     --wandb-project VLM_GRPO \
     --wandb-run MAZE_PPO_SFT \
     --use-wandb \
-    # --q4
+    --q4
 
 
